7b4b344e108f9f8b420ed5f8843fbe60e2ea0163
getbackwards <getbackwards@gmail.com>
2017-08-08 20:54:17 +1200

    added in a switch statement

--- js/aria-live-regions.js
+++ js/aria-live-regions.js
@@ -4,8 +4,32 @@
 // console.log(selectValue);
 
 function updateBirdInfo() {
-    // var x = document.getElementById("mySelect").value;
+
     const selectedValue = document.querySelector('#bird-selector').value;
-    document.querySelector("#bird-info").innerHTML = "You selected: " + selectedValue;
+
+    switch (selectedValue) {
+        case 'hawk':
+            birdInfo = "Some information about the hawk";
+            break;
+        case 'owl':
+            birdInfo = "Some information about the owl";
+            break;
+        case 'sparrow':
+            birdInfo = "Some information about the sparrow";
+            break;
+        case 'tui':
+            birdInfo = "Some information about the tui";
+            break;
+        default:
+            birdInfo = "Some information about the Eagle";
+    }
+    // var x = document.getElementById("mySelect").value;
+
+    // document.querySelector("#bird-info").innerHTML = "You selected: " + selectedValue;
+    document.querySelector("#bird-info").innerHTML = birdInfo;
     console.log(selectedValue);
+
+
 }
+
+
